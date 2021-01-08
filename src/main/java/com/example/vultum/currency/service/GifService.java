2b56dfc0fd3ea119c2b3e69d0317f50f5443e831
Vultum Volubiles <afuau@yandex.ua>
2021-01-08 05:28:02 +0300

    UPD: search gif

--- src/main/java/com/example/vultum/currency/service/GifService.java
+++ src/main/java/com/example/vultum/currency/service/GifService.java
@@ -1,4 +1,35 @@
 package com.example.vultum.currency.service;
 
+import com.example.vultum.currency.config.GifServiceConfiguration;
+import com.example.vultum.currency.dto.GifResource;
+import com.example.vultum.currency.dto.GifSearchResource;
+import feign.Feign;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.okhttp.OkHttpClient;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GifService {
+    private final GifServiceConfiguration configuration;
+    private final GifClient client;
+
+    public GifService(GifServiceConfiguration configuration) {
+        this.configuration = configuration;
+        client = Feign.builder()
+                .client(new OkHttpClient())
+                .encoder(new GsonEncoder())
+                .decoder(new GsonDecoder())
+                .target(GifClient.class, configuration.getEndpoint());
+    }
+
+    @SneakyThrows
+    public List<GifResource> search(String query){
+        GifSearchResource result = client.search(configuration.getApiKey(), query, 1);
+        GifResource resource = new GifResource(result.data.get(0));
+        return result.get();
+    }
 }
