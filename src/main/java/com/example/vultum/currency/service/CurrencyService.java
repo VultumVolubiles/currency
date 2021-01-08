19da1c9b13ac2b4988173cb21776d240a8e5a094
Vultum Volubiles <afuau@yandex.ua>
2021-01-08 06:16:26 +0300

    UPD: latest, yesterday, compare

--- src/main/java/com/example/vultum/currency/service/CurrencyService.java
+++ src/main/java/com/example/vultum/currency/service/CurrencyService.java
@@ -1,4 +1,52 @@
 package com.example.vultum.currency.service;
 
+import com.example.vultum.currency.config.CurrencyServiceConfiguration;
+import com.example.vultum.currency.dto.CurrencyResource;
+import com.example.vultum.currency.service.client.ExternalCurrencyServiceClient;
+import feign.Feign;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.okhttp.OkHttpClient;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
 public class CurrencyService {
+    private final CurrencyServiceConfiguration configuration;
+    private ExternalCurrencyServiceClient client;
+
+    public CurrencyService(CurrencyServiceConfiguration configuration) {
+        this.configuration = configuration;
+        client = Feign.builder()
+                .client(new OkHttpClient())
+                .encoder(new GsonEncoder())
+                .decoder(new GsonDecoder())
+                .target(ExternalCurrencyServiceClient.class, configuration.getEndpoint());
+    }
+
+    public CurrencyResource latest(String base) {
+        if (base == null)
+            return client.latest(configuration.getApiKey());
+        else
+            return client.latest(configuration.getApiKey(), base.toUpperCase());
+    }
+
+    public CurrencyResource yesterday(String base) {
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        if (base == null)
+            return client.historical(configuration.getApiKey(), date);
+        else
+            return client.historical(configuration.getApiKey(), date, base.toUpperCase());
+    }
+
+    public int compareRate(CurrencyResource res1, CurrencyResource res2, String rate) {
+        if (!(res1.hasRate(rate) && res2.hasRate(rate)))
+            throw new IllegalArgumentException("Invalid currency");
+
+        Double cur1 = res1.getRate(rate.toUpperCase());
+        Double cur2 = res2.getRate(rate.toUpperCase());
+        return Double.compare(cur1, cur2);
+    }
 }
